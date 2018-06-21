f19e993f1ed0498ada0fe15c9d1c3870f02fcd57
Newthrone <kent682@mail.ru>
2018-06-21 22:14:41 +0300

    Добавилправильный маркер на карту

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ showSlides(slideIndex);
 // for automatic slider
 function plusSlides() {
   slideIndex += 1;
-  showSlides(slideIndex);  
+  showSlides(slideIndex);
 }
 
 // Thumbnail image controls
@@ -17,32 +17,42 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("header-slider");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} ;
-  if (n < 1) {slideIndex = slides.length};
+  if (n > slides.length) {
+    slideIndex = 1
+  };
+  if (n < 1) {
+    slideIndex = slides.length
+  };
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
   // automatic slider
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
   setTimeout(plusSlides, 4000); // Change image every 2 seconds
 }
 
 // google map
 
 function myMap() {
-    var spb = {lat: 59.938606085305835, lng: 30.32306171777837};
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: spb,
-        zoom: 17
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-    var marker = new google.maps.Marker({
+  var spb = {
+    lat: 59.938606085305835,
+    lng: 30.32306171777837
+  };
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: {lat: 59.939207096405835,
+    lng: 30.31903571777837},
+    zoom: 17
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({
     position: spb,
-    map: map
-});
+    map: map,
+    icon: '/image/marker.png'
+  });
+  
 }
